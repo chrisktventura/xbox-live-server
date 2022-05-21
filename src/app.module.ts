3ba4f4bf9950e96cccaf55d5bdf95441c3c899a4
Christopher Ventura <chrisventura@outlook.com.br>
2022-05-21 20:26:59 -0300

    feat prisma: create module e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GenreModule } from './genre/genre.module';
 import { GamesModule } from './games/games.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GamesModule, GenreModule],
+  imports: [GamesModule, GenreModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
