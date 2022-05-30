3dc9d9e7ebd01034ce9291890a02ce6cc5829a4d
Christopher Ventura <chrisventura@outlook.com.br>
2022-05-30 18:56:06 -0300

    .

--- src/profile/entities/profile.entity.ts
+++ src/profile/entities/profile.entity.ts
@@ -1,11 +1,7 @@
-import { Game, User } from '@prisma/client';
-
 export class Profile {
   id?: string;
   name: string;
   image: string;
   updateAt?: Date;
   createAt?: Date;
-  user?: User;
-  games?: Game[];
 }
