91350c29b0a65ffc47e2d9cc03eaed594ea09d30
Christopher Ventura <chrisventura@outlook.com.br>
2022-06-07 20:06:04 -0300

    passport module in homepage module

--- src/homepage/homepage.module.ts
+++ src/homepage/homepage.module.ts
@@ -1,11 +1,19 @@
 import { PrismaModule } from './../prisma/prisma.module';
-
 import { Module } from '@nestjs/common';
 import { HomepageService } from './homepage.service';
 import { HomepageController } from './homepage.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [HomepageController],
   providers: [HomepageService],
 })
